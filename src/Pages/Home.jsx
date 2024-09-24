b9d6544c56ce7a07e554768284434b781b0b014c
Amit-Bongale <amitbongale14@gmail.com>
2024-09-24 19:57:13 +0530

    material ui initailized

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 function Home() {
+  function hi(){
+    alert("Hello")
+  }
   return (
-    <div className='text-3xl font-bold underline'>Home</div>
+    <div className='text-3xl font-bold underline'>
+      Home
+      <Button variant="contained">UI</Button>
+    </div>
   )
 }
 
