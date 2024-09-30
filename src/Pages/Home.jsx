598f88b67797c7e9c3ac0d73ed5296deff80d998
apoorvass <ssapoorva4@gmail.com>
2024-09-30 17:14:36 +0530

    Home page edited

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import Herosection from './Herosection'
 
 
 import { Link } from 'react-router-dom'
@@ -7,15 +7,47 @@ import { Link } from 'react-router-dom'
 function Home() {
 
   return (
-    <div className='text-3xl font-bold'>
-      
-      Home
+    // <div className='text-3xl font-bold'> 
+    //   Home
+    //   <div>
+    //     Choose Role : <Link to={'/chooserole'}> <Button  variant="contained"> choose </Button> </Link> 
+    //   </div>
 
-      <div>
-        Choose Role : <Link to={'/chooserole'}> <Button  variant="contained"> choose </Button> </Link> 
-      </div>
+    // </div>
+    <div >
+    <div className="bg-green text-xl font-semibold text-black flex items-center justify-around">
+
+        <div>
+            <Link to='/'> <div >
+                <h1>Hospital Management System</h1>
+                {/* <img className="logo" src={logo} alt="" /> */}
+            </div> </Link>
+        </div>
+
+        <div>
+
+            <div >
+                <Link to='/'>  <button className="mx-8 my-5 ">Home</button> </Link>
+                
+                <Link to='/Appointment'> <button className="mx-8 my-5 ">Appointment</button>  </Link>
+
+                <Link to='/login'> <button className="mx-8 my-5"> Login </button> </Link>
+
+                <Link to='/contactus'> <button className="mx-8 my-5 "> Contact us </button> </Link>
+
+                <Link to='/chooserole'> <button className="mx-8 my-5 "> Roles </button> </Link>
+
+
+            </div>
+            
+        </div>
+
+    <Herosection></Herosection>  
+    
 
     </div>
+    
+</div>
   )
 }
 
--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes , Route } from 'react-router-dom'
 
 import Home from '../Pages/Home'
+import Footer from '../Pages/Footersection'
 
 import ChooseRole from '../Pages/Role/ChooseRole'
 
