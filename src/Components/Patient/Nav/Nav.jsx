6a88509d5f8099c1041d8cdd00422da4f0228c90
apoorvass <ssapoorva4@gmail.com>
2024-10-01 09:20:47 +0530

    Home Page structure created

---
+++ src/Components/Patient/Nav/Nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+function Nav() {
+    return(
+        <div>
+            <div>
+            <Link to='/'> <div >
+                <h1>Hospital Management System</h1>
+                {/* <img className="logo" src={logo} alt="" /> */}
+            </div> </Link>
+        </div>
+
+        <div>
+
+            <div >
+                <Link to='/'>  <button className="mx-8 my-5 ">Home</button> </Link>
+                
+                <Link to='/Appointment'> <button className="mx-8 my-5 ">Appointment</button>  </Link>
+
+                <Link to='/login'> <button className="mx-8 my-5"> Login </button> </Link>
+
+                <Link to='/contactus'> <button className="mx-8 my-5 "> Contact us </button> </Link>
+
+                <Link to='/chooserole'> <button className="mx-8 my-5 "> Roles </button> </Link>
+
+
+            </div>
+            
+        </div>
+        </div>
+    )
+}
+export default Nav
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,54 +1,27 @@
 import React from 'react'
-import Herosection from './Herosection'
+import Nav from '../Components/Patient/Nav/Nav'
+import Herosection from '../Components/Patient/Home/Herosection'
+import Footersection from '../Components/Patient/Home/Footersection'
 
 
-import { Link } from 'react-router-dom'
+
 
 function Home() {
 
   return (
-    // <div className='text-3xl font-bold'> 
-    //   Home
-    //   <div>
-    //     Choose Role : <Link to={'/chooserole'}> <Button  variant="contained"> choose </Button> </Link> 
-    //   </div>
-
-    // </div>
+  
     <div >
-    <div className="bg-green text-xl font-semibold text-black flex items-center justify-around">
-
-        <div>
-            <Link to='/'> <div >
-                <h1>Hospital Management System</h1>
-                {/* <img className="logo" src={logo} alt="" /> */}
-            </div> </Link>
-        </div>
-
-        <div>
-
-            <div >
-                <Link to='/'>  <button className="mx-8 my-5 ">Home</button> </Link>
-                
-                <Link to='/Appointment'> <button className="mx-8 my-5 ">Appointment</button>  </Link>
-
-                <Link to='/login'> <button className="mx-8 my-5"> Login </button> </Link>
-
-                <Link to='/contactus'> <button className="mx-8 my-5 "> Contact us </button> </Link>
-
-                <Link to='/chooserole'> <button className="mx-8 my-5 "> Roles </button> </Link>
-
-
-            </div>
-            
-        </div>
-
+    
+    <Nav></Nav>
     <Herosection></Herosection>  
+    <Footersection></Footersection>
     
 
     </div>
     
-</div>
+
   )
 }
 
 export default Home
+
