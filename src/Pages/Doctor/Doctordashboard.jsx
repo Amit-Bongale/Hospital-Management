1e49af362d26d1337c6a4b643ec3c3a02e92b3ff
Amit-Bongale <amitbongale14@gmail.com>
2024-10-01 19:13:02 +0530

    doctor dashboard edited

--- src/Pages/Doctor/Doctordashboard.jsx
+++ src/Pages/Doctor/Doctordashboard.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
+
 import Doctornav from '../../Components/Doctor/Nav/Doctornav'
 import Appointments from '../../Components/Doctor/Dashboard/Appointments'
 
-
-import { useDispatch } from 'react-redux'
-import { doctorlogout } from '../../Redux/Doctor/Doctor'
-
 function Doctordashboard() {
-
-  const dispatch = useDispatch()
 
   return (
     <div class="flex">
-
       <Doctornav/>
       <div>
         <Appointments/>
