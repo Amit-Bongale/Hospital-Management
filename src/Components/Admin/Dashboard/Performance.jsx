8be1ddf445a129e21049c04fd3961779bb717c7c
Amit-Bongale <amitbongale14@gmail.com>
2024-10-05 00:03:34 +0530

    home nav designed

--- src/Components/Admin/Dashboard/Performance.jsx
+++ src/Components/Admin/Dashboard/Performance.jsx
@@ -4,6 +4,7 @@ import { LineChart } from '@mui/x-charts/LineChart';
 function Performance() {
   return (
     <div className='border-2 p-2 rounded-3xl max-h-fit mt-4' >
+      <h3 className='font-bold text-2xl ml-4 mt-4'>Total Visitors</h3>
       <LineChart
       xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
       series={[
@@ -14,9 +15,8 @@ function Performance() {
         },
       ]}
       width={1100}
-      height={400}
+      height={360}
     />
-
     </div>
     
   )
