4a461c4f4f53157cd596cce373f549bce1a7377a
anushaanadiger <anushaanadiger@gmail.com>
2024-10-09 15:41:34 +0530

    Doctorschedule done

--- src/Components/Doctor/Dashboard/Doctorschedule.jsx
+++ src/Components/Doctor/Dashboard/Doctorschedule.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Schedule(){
     return(
         <div class="ml-72 mr-12">
-        <h1 className="font-bold text-xl p-4">Schedule Available</h1>
+        <h1 className="font-bold text-xl p-4 text-center mt-10">Doctor Schedule</h1>
 
       <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <table class="w-auto table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 
           <thead class="text-sm text-gray-700 uppercase bg-gray-50 text-center">
             <tr>
@@ -47,13 +47,51 @@ function Schedule(){
                 <td class="px-6 py-4"> Checkup </td>
                 <td class="px-6 py-4"> Rounds </td>
                 
+              </tr>
+            </tbody>
+        </table>
+      </div>
 
+      
+      <div class="ml-64 mt-36">
+      <h1 className="font-bold text-xl p-4 ml-72">Doctor Availability</h1>
+
+        <table class="w-auto shadow-md text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+
+          <thead class="text-sm text-gray-700 w-auto uppercase bg-gray-50 text-center">
+            <tr>
+              <th scope="col" class="px-6 py-3"> Sunday </th>
+              <th scope="col" class="px-6 py-3"> Monday </th>
+              <th scope="col" class="px-6 py-3"> Tuesday </th>
+              <th scope="col" class="px-6 py-3"> Wednesday </th>
+              <th scope="col" class="px-6 py-3"> Thursday </th>
+              <th scope="col" class="px-6 py-3"> Friday </th>
+              <th scope="col" class="px-6 py-3"> Saturday </th>
+             
+
+            </tr>
+          </thead>
+
+            <tbody>
+              <tr class="bg-white border-b text-sm text-gray-700 w-auto dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-center">
+
+                {/* <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"> */}
+                 {/* </td> */}
+                <td class="px-6 py-4"> Holiday </td>
+                <td class="px-6 py-4"> Available </td>
+                <td class="px-6 py-4"> Available </td>
+                <td class="px-6 py-4"> Available </td>
+                <td class="px-6 py-4"> Available </td>
+                <td class="px-6 py-4"> Available </td>
+                <td class="px-6 py-4"> Half Day </td>
                 
 
               </tr>
             </tbody>
         </table>
       </div>
+
+
     </div>
 
     )
