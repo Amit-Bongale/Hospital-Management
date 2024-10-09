11232e29f863136d4709e7907229bd42b69a9904
lekhanamaridi <lekhanamaridi444@gmail>
2024-10-09 15:58:31 +0530

    edited staff components

--- src/Components/Staff/Admit/AdmitPatientInfo.jsx
+++ src/Components/Staff/Admit/AdmitPatientInfo.jsx
@@ -9,7 +9,23 @@ function AdmitPatientInfo() {
   let [admitpatient,setadmitpatient] = useState(false)
   return (
     <div>
-        <div className="flex">
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg ml-32">
+        <table class="w-full table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+          <thead class="text-sm text-gray-700 uppercase bg-gray-50 text-center">
+            <tr>
+              <th scope="col" class="px-6 py-3"> PATIENT NAME </th>
+              <th scope="col" class="px-6 py-3"> GENDER </th>
+              <th scope="col" class="px-6 py-3"> PHONE NUMBER </th>
+              <th scope="col" class="px-6 py-3"> AGE </th>
+              <th scope="col" class="px-6 py-3"> DOCTOR NAME </th>
+              <th scope="col" class="px-6 py-3"> STAFF ID </th>
+              <th scope="col" class="px-6 py-3"> WARD NUMBER </th>
+              <th scope="col" class="px-6 py-3"> BED NUMBER </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+        <div className="flex justify-center">
             <span className="grid items-start px-2 text-xl font-medium lg:px-4">
                 <button
                 onClick={() => setadmitpatient(true)}
