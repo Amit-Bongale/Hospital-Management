48425ac2c5d241326c0ce96f308336857c354b08
Amit-Bongale <amitbongale14@gmail.com>
2024-10-14 18:26:35 +0530

    nav fixed

--- src/Components/Patient/Nav/Nav.jsx
+++ src/Components/Patient/Nav/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
   let [appointment, setappointment] = useState(false)
   return (
     <nav>
-      <div className="flex items-center justify-around bg-blue-400 text-white text-xl font-semibold">
+      <div className="flex items-center justify-around bg-blue-500 text-white font-semibold">
         <div>
           <Link to="/">
             <div>
@@ -16,33 +16,32 @@ function Nav() {
             </div>
           </Link>
         </div>
-      <div class="flex">
-        <div class="my-6 flex">
+
+        <div class="my-5">
             <Link to="/">
-              <h1 className="mr-10 ml-32  text-xl font-semibold">Home</h1>
+              <button className="mr-10 ml-20">Home</button>
             </Link>
 
             <Link to="/contactusform">
-              <h1 className=" mr-10 text-xl font-semibold"> Contact Us </h1>
+              <button className=" mr-10"> Contact Us </button>
             </Link>
 
             <Link to="/chooserole">
-              <h1 className="mr-10 text-xl font-semibold"> Roles </h1>
+              <button className="mr-10"> Roles </button>
             </Link>
         
 
         <Link to="/loginform">
-            <h1 className="mr-10 text-xl font-semibold"> Login </h1>
+            <button className="mr-10 ml-10"> Login </button>
         </Link>
 
         </div>
         <div>
-              <button onClick={() => setappointment(true)} className="px-6 py-2 mt-5 rounded-full bg-white text-black mr-20">Book Appointment</button>
+              <button onClick={() => setappointment(true)} className="px-6 py-2 rounded-full bg-white text-black ">Book Appointment</button>
         </div>
 
         {appointment ?<AppointmentForm setappointment={setappointment}/>
-        :<></>}   
-        </div>    
+        :<></>}       
       </div>
     </nav>
   );
