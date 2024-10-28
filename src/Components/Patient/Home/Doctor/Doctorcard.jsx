855324841aacdd7cf6920e139120a32254654f3f
Amit-Bongale <amitbongale14@gmail.com>
2024-10-28 20:05:51 +0530

    doctor card api fixed

--- src/Components/Patient/Home/Doctor/Doctorcard.jsx
+++ src/Components/Patient/Home/Doctor/Doctorcard.jsx
@@ -7,10 +7,10 @@ function Doctorcard() {
 
   useEffect(()=>{ 
     try {
-      fetch(`${process.env.REACT_APP_API_URL}/doctor/alldoctors`)
-      .then((res)=>{res.json()})
-      .then((data)=>{setdoctorinfo(data)})
-      .catch((error)=>{console.log('error fetching data',error)})
+      fetch(`${process.env.REACT_APP_API_URL}/doctor/alldoctors` , { method: "POST" })
+      .then((res)=> res.json())
+      .then((data)=>{ setdoctorinfo(data); console.log(data) })
+      .catch((error)=>console.log('error fetching data',error))
       
     } catch (error) {
       console.log('error:', error)
@@ -27,21 +27,21 @@ function Doctorcard() {
       </div>
       <div class="flex items-center mt-2.5 mb-2 ">
         <div class="flex flex-wrap p-1 m-1.5 ">
-          {doctorinfo.map((doctor)=>(
+          { doctorinfo.map((doctor)=>(
 
             <div class="ml-28 mr-16">
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvxv-aOP1Kg25NKYqPdJj3hx0jmJEydfAGYCaLywnW6kiyFkrYsockTqIZOArz2v9SRuY&usqp=CAU"
-              alt=""
+              src={doctor.image}
+              alt="doctor profile"
+              className="w-56"
             />
-            <h1 class="mx-12 my-2"> Name:Dr Anusha</h1>
+            <h1 class="mx-12 my-2"> {doctor.name} </h1>
             <div>
               <button
             
                 className="mx-11 my-2 "
               >
-                {" "}
-                Book Appointment{" "}
+                Book Appointment
               </button>
             </div>
           </div>
