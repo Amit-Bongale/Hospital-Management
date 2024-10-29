8ea3ce07f8cf03ed7dc302e219a358d142c9cfad
lekhanamaridi <lekhanamaridi444@gmail>
2024-10-29 19:51:39 +0530

    forms in staff components are edited

--- src/Components/Staff/Test/PatientTestInfo.jsx
+++ src/Components/Staff/Test/PatientTestInfo.jsx
@@ -11,13 +11,14 @@ function PatientTestInfo() {
         <table class="w-full table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
           <thead class="text-sm text-gray-700 uppercase bg-gray-50 text-center">
             <tr>
-              <th scope="col" class="px-6 py-3"> AADHAR NUMBER  </th>
+              <th scope="col" class="px-6 py-3"> AADHAAR NUMBER  </th>
               <th scope="col" class="px-6 py-3"> DOCTOR ID </th>
               <th scope="col" class="px-6 py-3"> STAFF ID </th>
               <th scope="col" class="px-6 py-3"> TEST NAME </th>
             </tr>
           </thead>
-          <tbody>
+
+          <tbody class="text-center">
             <tr>
               <td scope="col" class="px-6 py-3"> 587458966325 </td>
               <td scope="col" class="px-6 py-3"> Appu </td>
