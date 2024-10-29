2eabdeddc50a22e6e6d9dc753939fb6234f2dabf
lekhanamaridi <lekhanamaridi444@gmail>
2024-10-29 15:47:14 +0530

    test information edited

--- src/Components/Staff/Test/PatientTestInfo.jsx
+++ src/Components/Staff/Test/PatientTestInfo.jsx
@@ -19,16 +19,16 @@ function PatientTestInfo() {
           </thead>
           <tbody>
             <tr>
-              <th scope="col" class="px-6 py-3"> 587458966325 </th>
-              <th scope="col" class="px-6 py-3"> Appu </th>
-              <th scope="col" class="px-6 py-3"> 65 </th>
-              <th scope="col" class="px-6 py-3"> blood test </th> 
-              <th scope="col" class="px-6 py-3"> 
+              <td scope="col" class="px-6 py-3"> 587458966325 </td>
+              <td scope="col" class="px-6 py-3"> Appu </td>
+              <td scope="col" class="px-6 py-3"> 65 </td>
+              <td scope="col" class="px-6 py-3"> blood test </td> 
+              <td scope="col" class="px-6 py-3"> 
                 <button scope="col" class="px-6 py-3"  onClick={() => settestpatient(true)} className="text-blue-600 hover:cursor-pointer"> Edit </button>
-              </th>
-              <th scope="col" class="px-6 py-3">  
+              </td>
+              <td scope="col" class="px-6 py-3">  
                 <button scope="col" class="px-6 py-3"  className="text-red-600 hover:cursor-pointer"> Delete </button>
-              </th>
+              </td>
             </tr>
           </tbody>
         </table>
