92140dbc3d691e7135c187324baf84f2187534e3
Amit-Bongale <amitbongale14@gmail.com>
2024-10-30 11:57:54 +0530

    queue featuere added

--- src/Components/Doctor/Dashboard/Appointments.jsx
+++ src/Components/Doctor/Dashboard/Appointments.jsx
@@ -1,46 +1,63 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 //import { Link } from 'react-router-dom'
 
-import Viewpatient from '../Patientdetails/Viewpatient'
-
+import Viewpatient from "../Patientdetails/Viewpatient";
 
 function Appointments() {
+  let [view, setview] = useState(false);
 
-  let [view,setview] = useState(false)
+  let [queueinfo, setqueueinfo] = useState([]);
+
+
+  useEffect(()=> {
+    try {
+      fetch(`${process.env.REACT_APP_API_URL}/queue/allpatient`, {
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.message) {
+            console.log(data.message);
+            alert(data.message);
+            setqueueinfo(data);
+          }
+          console.log(data);
+        })
+        .catch((error) => console.log("Fetching Error:", error));
+    } catch (error) {
+      console.log("error :", error);
+    }
+  },[view])
+
 
   return (
     <div className=" w-full mt-10">
-      <div class=" w-full flex text-lg font-bold ml-20 text-gray-700 border-cyan-400 border-2 rounded-lg text-center h-20 ">
-
-          <div class = "mr-20 ml-20 mt-4" >
+      {queueinfo.map((queue) => (
+        <div class=" w-full flex text-lg font-bold ml-20 text-gray-700 border-cyan-400 border-2 rounded-lg text-center h-20 ">
+          <div class="mr-20 ml-20 mt-4">
             <p>NAME</p>
-            <p class="text-base font-medium">Anusha</p>
+            <p class="text-base font-medium">{queue.name} </p>
           </div>
 
-          <div class = "mr-20 ml-20 mt-4" >
+          <div class="mr-20 ml-20 mt-4">
             <p>GENDER</p>
-            <p class="text-base font-medium ">Male</p>
+            <p class="text-base font-medium ">{queue.gender} </p>
           </div>
-         
-          <div class = "mr-20 ml-20 mt-4" >
+
+          <div class="mr-20 ml-20 mt-4">
             <p>DISEASE</p>
-            <p class="text-base font-medium">Cough</p>
+            <p class="text-base font-medium">{queue.disease} </p>
           </div>
 
-          <div class = "mr-10 ml-20 mt-7" >
-            <button
-            onClick={() => setview(true)}>
-            VIEW
-            </button>
-            
+          <div class="mr-10 ml-20 mt-7">
+            <button onClick={() => setview(true)}>VIEW</button>
           </div>
-          {view ?<Viewpatient setview={setview}/>
-            :<></>}
-
-      </div>
+        </div>
+      ))}
+      {view ? <Viewpatient setview={setview} /> : <></>}
     </div>
-  )
+  );
 }
 
-export default Appointments
+export default Appointments;
