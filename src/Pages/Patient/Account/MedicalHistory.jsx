ff152cde757e180e8ca2c3e210822eec8d39bb59
apoorvass <ssapoorva4@gmail.com>
2024-11-14 15:19:36 +0530

    patient medical history page is done

--- src/Pages/Patient/Account/MedicalHistory.jsx
+++ src/Pages/Patient/Account/MedicalHistory.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import AccountNav from '../../../Components/Patient/Nav/AccountNav'
+import PatientMedicalHistory from '../../../Components/Patient/MedicalHistory/PatientMedicalHistory'
 
 
 function MedicalHistory() {
   return (
     <div className='flex gap-4'>
         <AccountNav/>
-        MedicalHistory
+       <div >
+        <PatientMedicalHistory></PatientMedicalHistory>
+       </div>
     </div>
   )
 }
