3b1f9a643d7ac58effd26c549736a6c1ea8beb87
Amit-Bongale <amitbongale14@gmail.com>
2024-12-01 23:21:20 +0530

    Revenue stats added

--- src/Components/Admin/Dashboard/ActivePatients.jsx
+++ src/Components/Admin/Dashboard/ActivePatients.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useState , useEffect } from 'react';
 
 import { Gauge } from '@mui/x-charts/Gauge';
 
 function ActivePatients() {
+
+  let [activepatients , setactivepatients] = useState()
+
+  useEffect(() => {
+    try {
+      fetch(`${process.env.REACT_APP_API_URL}/queue/active`)
+      .then((res) => res.json())
+      .then((data) => {
+        setactivepatients(data)
+      })
+      .catch((err) => console.log("Error fetching data" , err))
+    } catch (error) {
+      console.log("Error:" , error)
+    }
+  },[activepatients])
+
   return (
     <div className='border-2 p-8 rounded-3xl  max-h-fit'>
-      <h1 className='font-bold text-2xl text-center'>Patients Active</h1>
+      <h1 className='font-bold text-2xl text-center'>Patients Queue</h1>
       <Gauge
-      value={20}
+      value={activepatients}
       width={200} height={150}
       startAngle={-90} endAngle={90}
       valueMin={0}
--- src/Pages/Admin/AdminDashboard.jsx
+++ src/Pages/Admin/AdminDashboard.jsx
@@ -6,24 +6,24 @@ import ActiveDoctors from '../../Components/Admin/Dashboard/ActiveDoctors'
 import ActivePatients from '../../Components/Admin/Dashboard/ActivePatients'
 import ActiveStaff from '../../Components/Admin/Dashboard/ActiveStaff'
 import ActiveRooms from '../../Components/Admin/Dashboard/ActiveRooms'
+import TotalIncome from '../../Components/Admin/Dashboard/TotalIncome'
 import Performance from '../../Components/Admin/Dashboard/Performance'
 
 function AdminDashboard() {
 
   return (
-    <div className='flex'>
+    <div className='flex gap-2'>
       <Adminnav/>
-      <div className='ml-4'>
 
-        <div className=' flex flex-wrap gap-6'>
+      <div className='ml-4'>
+        <div className=' flex flex-wrap gap-6 m-2'>
           <ActiveDoctors/>
           <ActiveStaff/>
           <ActivePatients/>
           <ActiveRooms/>
         </div>
-
+        <TotalIncome/>
         <Performance/>  
-
       </div>
 
     </div>
